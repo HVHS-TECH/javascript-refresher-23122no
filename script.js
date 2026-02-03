aa3704b1e1d7ded4be58ab073feac1fc9aae68c0
23122no <23122no@hvhs.school.nz>
2026-02-03 15:13:19 +1300

    js linked

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Congratulations!\nYou got the script running!")
+let userInput;
+
+function submitDetails () {
+    if (document.getElementById('f_userInput').checkValidity()) {
+        userInput = document.getElementById('i_input').value;
+        userInput = userInput.trim();
+        welcomeMessage.textContent = userInput;
+    } 
+}
+
+function showAlert () {
+    alert("Welcome!");
+}
