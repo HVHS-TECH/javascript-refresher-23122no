af7cdeb6dccc02aa2a0e7cfb31a32cb1073a490c
23122no <23122no@hvhs.school.nz>
2026-02-05 10:58:33 +1300

    testing commit

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let userInput;
 
 function submitDetails () {
+    console.log("Submit Pressed");
     if (document.getElementById('f_userInput').checkValidity()) {
+        console.log("Form Valid!");
         userInput = document.getElementById('i_input').value;
-        userInput = userInput.trim();
         welcomeMessage.textContent = userInput;
-    } 
+    } else {
+        console.log("Form Invalid");
+    }
 }
 
 function showAlert () {
